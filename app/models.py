69587808852d7c68c6a3ab56e05b8f76ee8ea0d9
Damelia Coleman <dameliac@outlook.com>
2025-04-08 00:15:20 -0500

    started exercise 2

--- app/models.py
+++ app/models.py
@@ -1,1 +1,13 @@
 # Add any model classes for Flask-SQLAlchemy here
+from . import db
+from datetime import datetime
+
+class Movie(db.Model):
+    #id = db.Column(db.Integer, primary_key=True)
+    #username = db.Column(db.String(80), unique=True)
+    #email = db.Column(db.String(120), unique=True)
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(555), unique=True)
+    description = db.Column(db.Text, nullable=True)
+    poster = db.Column(db.String(255), nullable=True)
+    created_at = db.Column(db.DateTime, default=datetime.timezone.utc, nullable=False)
